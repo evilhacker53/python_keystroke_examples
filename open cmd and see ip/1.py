a01b00a777df373fa37b3e599663eedac68434c2
evilhacker53 <37243893+evilhacker53@users.noreply.github.com>
2019-06-20 17:18:11 +0530

    Add files via upload

---
+++ open cmd and see ip/1.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Key, Controller
+import time
+
+
+keyboard = Controller()
+
+keyboard.press(Key.cmd)
+keyboard.press('r')
+time.sleep(1)
+keyboard.release(Key.cmd)
+keyboard.release('r')
+time.sleep(1)
+keyboard.type('cmd')
+time.sleep(1)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(2)
+keyboard.type('ipconfig')
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)

